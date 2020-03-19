7f6e21fdb1a2b3afe29f9e6ddf69c9a50b60a0e7
Korneevnm <Korneevnm@yandex.ru>
2020-03-19 07:59:28 +0400

    Updates

---
+++ precache-manifest.34998eaffac6aa9a288ab80996aa37bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5451fc2aaa9a8afde1c672449b9a5c52",
+    "url": "/cryptocurrency-converter/index.html"
+  },
+  {
+    "revision": "6211aa280b7e090ff081",
+    "url": "/cryptocurrency-converter/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "2d0b7685c7b42eb066eb",
+    "url": "/cryptocurrency-converter/static/css/main.0389423c.chunk.css"
+  },
+  {
+    "revision": "6211aa280b7e090ff081",
+    "url": "/cryptocurrency-converter/static/js/2.ad308eea.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/cryptocurrency-converter/static/js/2.ad308eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0b7685c7b42eb066eb",
+    "url": "/cryptocurrency-converter/static/js/main.b08083e7.chunk.js"
+  },
+  {
+    "revision": "603ac710f001d5567565",
+    "url": "/cryptocurrency-converter/static/js/runtime-main.55aafde8.js"
+  }
+]);
