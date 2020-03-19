1bd5edb424e7c0d778218909cec94e5ab433a7ec
Korneevnm <Korneevnm@yandex.ru>
2020-03-19 08:09:31 +0400

    Add readme and change api key

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -7,7 +7,7 @@ const axiosConfig = axios.create({
   baseURL: `${cors}${url}`
 });
 
-export const apiKey = '0dfc3175-2e72-4d3c-81bc-bb72416933e6';
+export const apiKey = '3db9eb8c-c320-4966-8516-1e84cd34b48e';
 export const apiKey2 = '61e96c3a-4935-48aa-81ac-a23e665fc5a2';
 export const apiKey3 = '482d902c-cc4c-4e2a-9f32-a4d2fa80e25a';
 export default axiosConfig;
